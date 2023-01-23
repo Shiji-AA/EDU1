89ca9d1ada468d92cae9aa14b7a12fb5cdcc4dc6
Shiji-AA <shijihcl@gmail.com>
2023-01-23 23:59:17 +0530

    alertconsole.js

---
+++ Javascript/alertconsole.js
@@ -0,0 +1,6 @@
+/*console.log() =>debugging or to display output*/
+/*alert() => for notification*/
+/*confirm() =>to check whether yes or no*/
+/*prompt() => to take user input*/
+
+alert("Form submitted")
