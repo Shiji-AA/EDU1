a5032eca47997d4d41ef85648763286f109aac17
Shiji-AA <shijihcl@gmail.com>
2023-01-24 12:35:56 +0530

    stringliterals.js

--- Javascript/letConst.js
+++ Javascript/letConst.js
@@ -1,15 +1,35 @@
-var => we can redeclare and reassign.
+/*var => we can redeclare and reassign.
 let => we cannot redeclare but can reassign.
-const=> we cannot redeclare nor reassign
+const=> we cannot redeclare nor reassign*/
 
 var a = 10
 var a // declaration
 a = 10 //assignment
+
+//var
+
 var a = 11
     a = 12
-    a // assignment
-
     console.log(a);
+
+//reassign
+var a = 11;
+    a = 12;
+    console.log(a);     
+    
+//redeclare
+var a = 11;
+var a = 12;
+console.log(a); 
+
+//let
+
+//reassign
+var a = 11;
+    a = 12;
+    console.log(a);   
+
+
 
 
 
---
+++ Javascript/stringliterals.js
@@ -0,0 +1,8 @@
+var mname = "The Avengers";
+var type = "action movie";
+var category = "hollywood";
+var rating = 4.5;
+var output =" An "+ mname + "is an"+ type + "with rating of "+ rating +"and from the "+ category+"."
+
+
+
