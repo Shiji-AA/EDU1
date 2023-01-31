d66f63cfa852a08fc9cbb6615ba9eee9d8b6e105
Shiji-AA <shijihcl@gmail.com>
2023-01-31 12:23:57 +0530

    stringoperations.js

---
+++ Javascript/Calculator/calc.js
@@ -0,0 +1,24 @@
+/*function calc(option){
+    let a = document.getElementById("first").value;
+    let b = document.getElementById("second").value;
+    let output;
+    if(option == "add"){
+        output = `Sum of number is ${Number(a) + Number(b)}`      
+    }else{
+        output = `Sub of number is ${Number(a) - Number(b)}` 
+    }
+
+    document.getElementsByClassName("out")[0].innerText = output
+}
+*/
+
+
+
+function add() {
+   let a = document.getElementById("first").value;
+   let b = document.getElementById("second").value;
+      let output = Number(a) + Number(b);
+   document.getElementsByClassName("out")[0].innerText = `Sum of number is ${output}`;
+ }
+
+
--- Javascript/stringoperations.js
+++ Javascript/stringoperations.js
@@ -48,6 +48,48 @@ var city = "paRIs"
 console.log(city.charAt(0).toUpperCase()+ city.slice(1).toLowerCase());
 //Paris
 
+//replaceAll(pattern,replacement);  syntax
+
+var uname = "  kalyani. "
+console.log(uname.replaceAll (" ","/"));
+
+const string = "Hello everyone hi hi";
+let pattern = "hi";
+let replacement = "hey";
+let new_string = string.replaceAll(pattern,replacement);
+console.log(new_string);
+
+
+const a1 = "Hello Everyone good good day";
+console.log(a1.replace(/ /g,"?"));
+console.log(a1.replace(/good/g,"?"));
+
+//split
+var a = "https://github.com/Shiji-AA"
+console.log(a.split("/"));
+
+var a = "Javascript"
+a.split("");
+console.log(a.split(""));
+//(10) ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
+
+//Array to string
+
+var b = ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't'];
+console.log(b.toString());
+// J,a,v,a,s,c,r,i,p,t
+
+//To avoid commas
+console.log(b.toString().replaceAll(",",""));
+
+
+
+
+
+
+
+
+
 
 
 
