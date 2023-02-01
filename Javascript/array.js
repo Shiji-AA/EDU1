efeaa921544f6d3e2eb77e40c5d0b9aaf306c4b4
Shiji-AA <shijihcl@gmail.com>
2023-02-01 12:51:03 +0530

    array.js

--- Javascript/array.js
+++ Javascript/array.js
@@ -59,16 +59,63 @@ console.log(a.pop()); //venice
 //How to remove First Option in an ARRAY
 
 // unshift -> add as first value in Array
-var a = [
-    "Delhi",
-    "Mumbai",
-    "Helsinki",
-    "London",
-    "Amsterdam",
-    "Boston",
-    "Venice",
-  ];
-  
+var a = [ "Delhi", "Mumbai","Helsinki", "London", "Amsterdam","Boston","Venice"];
+console.log(a.unshift("Dubai"));
+  // ["Dubai","Delhi", "Mumbai", "Helsinki", "London", "Amsterdam", "Boston", 'Venice']
+//     0      1         2         3          4            5          6          7
+
+//["Delhi", "Mumbai", "Helsinki", "London", "Amsterdam", "Boston", 'Venice']
+
+
+console.log(a.shift()); //"Dubai"(like pop it will show only the removed part)
+
+//slice
+
+var a =["Dubai","Delhi", "Mumbai", "Helsinki", "London", "Amsterdam", "Boston", 'Venice'];
+
+console.log(a.slice(1));
+
+//(7) ['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston', 'Venice']
+
+console.log(a.slice(2 ,5));
+(3) ['Mumbai', 'Helsinki', 'London']
+
+console.log(a.slice(0,-2));
+(6) ['Dubai', 'Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam']
+
+
+//splice(startIndex, DeleteCount, values)
+
+var a  = ["Dubai","Delhi", "Mumbai", "Helsinki", "London", "Amsterdam", "Boston", "Venice"];
+//          0      1         2         3          4            5          6          7
+
+//remove 2 values from index 2
+console.log(a.splice(2, 2));
+//["Mumbai", "Helsinki"]
+// ["Dubai","Delhi", "London", "Amsterdam", "Boston", "Venice"]
+
+
+//remove 0 values but add 2 values on index 3
+console.log(a.splice(3, 0, 'Pune', 'Paris'));
+//["Dubai","Delhi", "London", 'Pune', 'Paris',"Amsterdam", "Boston", "Venice"]
+
+var a  = ["Dubai","Delhi", "Mumbai", "Helsinki", "London", "Amsterdam", "Boston", "Venice"];
+//remove 1 value on index 1 and add 2 values
+console.log(a.splice(1,1,"Innsburg", "France"));//output will show only deleted parts..
+
+//["Dubai","Innsburg", 'France', "London", 'Pune', 'Paris',"Amsterdam", "Boston", "Venice"]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
