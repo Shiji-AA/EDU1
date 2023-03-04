78e4fa93348936aa8f738eb9adf6253e77e66a8f
Shiji-AA <shijihcl@gmail.com>
2023-03-04 12:15:01 +0530

    assignment3.js

---
+++ Javascript/assignment3.js
@@ -0,0 +1,100 @@
+//Part 1 first
+
+class restaurantManager {
+    
+    //Array of objects
+    restaurantList =[
+        {
+    id:1,
+    restaurantName: 'KFC',
+    address: 'Anand Vihar',
+    city:'Delhi'
+    },
+
+    {
+    id:2,
+    restaurantName: 'Domino',
+    address: 'Savita Vihar',
+    city:'Mumbai'
+    }, 
+
+    {
+    id:3,
+    restaurantName: 'Burger King',
+    address: 'Civil Lane',
+    city:'Pune'    
+    },
+    {
+    id:4,
+    restaurantName: 'Subway',
+    address: 'Contonment',
+    city:'Mumbai'    
+    },
+    
+    ] ;
+
+    //part1-second
+
+    printAllRestaurantNames=()=>{
+        return this.restaurantList.map((data) => {
+            return data.restaurantName;
+        }) ;
+    };
+    
+    //part1-third
+
+         filterRestaurantByCity = (cityname) => {
+            return this.restaurantList.filter(restaurant => restaurant.city == cityname)
+         }
+
+    }
+
+    const restaurantObject = new restaurantManager();
+    const output1 = restaurantObject.printAllRestaurantNames();
+    const output2 = restaurantObject.filterRestaurantByCity("Delhi");
+    console.log(output1);
+    console.log(output2);
+
+
+    //Part 2-first
+
+    orderData = { 
+    'Below 500': 20, 
+    '500-1000': 29,
+    '1000-1500': 30, 
+    '1500-2000': 44, 
+    'Above 2000': 76
+    };
+0
+    const values = Object.values(orderData);
+    console.log(values);
+    const total = values.reduce((prev,next) =>prev + next);
+    console.log(total);
+
+     //Part 2-second
+
+   //const headers = Object.keys(orderData).length;
+   //console.log(headers);
+    const headers = Object.keys(orderData);
+    console.log(headers);
+
+
+    //Part 2-third
+     let arr =[];
+     const response = headers.map((item,index) => {
+        const obj= {
+            'id': index + 1,
+            'order': item,
+            'orderpercentage': ((orderData[item]/total)*100).toFixed(2),
+            'restaurant':'Punjabi Tadka'
+        }
+     arr.push(obj);
+     })
+     console.log(arr);
+
+
+     //end
+
+
+
+
---
+++ Javascript/assignment4.js
@@ -0,0 +1,52 @@
+//1a. Average rating for all the restaurants. 
+
+var ratingData = [
+    {restaurant: 'KFC', rating:5},
+    {restaurant: 'Burger King', rating:4},
+    {restaurant: 'KFC', rating:3},
+    {restaurant: 'Domino', rating:2},
+    {restaurant: 'Subway', rating:3},
+    {restaurant: 'Domino', rating:1},
+    {restaurant: 'Subway', rating:4},
+    {restaurant: 'Pizza Hut', rating:5}
+    ]
+    
+    //distinct restaurant
+    let distinct = [];
+  for (i = 0; i < ratingData.length; i++) {
+  if (distinct.indexOf(ratingData[i].restaurant) === -1) {
+    distinct.push(ratingData[i].restaurant);
+  }
+}
+
+let unique = ["KFC", "Burger King", "Domino", "Subway", "Pizza Hut"];
+//              0       1             2         3           4
+let avgRating = [];
+let outRating = 0;
+let count = 0;
+
+for (i = 0; i < unique.length; i++) {
+  for (j = 0; j < ratingData.length; j++) {
+    if (unique[i] == ratingData[j].restaurant) {
+      outRating += ratingData[j].rating;
+      count = count + 1;
+    }
+  }
+
+  let output = Number(outRating) / Number(count);
+  let myObj = {};
+  myObj.restaurant = unique[i];
+  myObj.avgRating = output;
+  avgRating.push(myObj);
+  count = 0;
+  outRating = 0;
+}
+console.log(avgRating);
+
+
+//1b.List of all restaurants with average rating greater than or equal to 4. 
+
+const output2 = avgRating.filter(item => item.avgRating >= 4);
+console.log(output2);
+
+//end
