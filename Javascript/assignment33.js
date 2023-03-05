d9b9fdb10a0c0f45114bcf802ac899beb9ed0218
Shiji-AA <shijihcl@gmail.com>
2023-03-05 16:18:42 +0530

    changes

--- Javascript/assignment33.js
+++ Javascript/assignment33.js
@@ -60,17 +60,27 @@ class restaurantManager {
     //Part 2-first
 
     orderData = { 
-    'Below 500': 20, 
+    'Below 500':20 , 
     '500-1000': 29,
     '1000-1500': 30, 
     '1500-2000': 44, 
-    'Above 2000': 76
+    'Above 2000':76
     };
-0
-    const values = Object.values(orderData);
+ 
+    
+    let total = 0;
+    for(key in orderData) {
+    total += Number(orderData[key]);
+ }
+ console.log(total);
+ 
+
+
+
+   /* const values = Object.values(orderData);
     console.log(values);
     const total = values.reduce((prev,next) =>prev + next);
-    console.log(total);
+    console.log(total);*/
 
      //Part 2-second
 
@@ -81,6 +91,43 @@ class restaurantManager {
 
 
     //Part 2-third
+
+
+    function getPercent(orderData) {
+        //calculate total
+        let total = 0;
+ for(key in orderData) {
+    total += Number(orderData[key]);
+ }
+ 
+        //calculate the percent
+        let output = [];
+        let count = 0;
+        for (key in orderData) {
+          let percentValue = ((orderData[key] / total) * 100).toFixed(2);
+          count = count + 1;
+          let myObj = {};
+          myObj.id = count;
+          myObj.order = key;
+          myObj.order_percentage = `${percentValue}%`;
+          myObj.restaurant = "Punjabi Tadka";
+          output.push(myObj);
+        }
+        return output;
+      };
+      console.log(getPercent(orderData));
+      
+      
+
+
+
+      
+    //[{id: 1,order: "Below 500",order_percentage: "10.05%",restaurant: "Punjabi Tadka"} 
+      // {id: 2, order: '500-1000', order_percentage: '14.57%', restaurant: 'Punjabi Tadka'}
+      // {id: 3, order: '1000-1500', order_percentage: '15.08%', restaurant: 'Punjabi Tadka'}
+      // {id: 4, order: '1500-2000', order_percentage: '22.11%', restaurant: 'Punjabi Tadka'} 
+      // {id: 5, order: 'Above 2000', order_percentage: '38.19%', restaurant: 'Punjabi Tadka'}]
+      
      let arr =[];
      const response = headers.map((item,index) => {
         const obj= {
