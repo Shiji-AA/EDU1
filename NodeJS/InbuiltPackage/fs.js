8f13ea437caa163bb297926deff1ad0b05b08abe
Shiji-AA <shijihcl@gmail.com>
2023-03-09 14:20:55 +0530

    changes

--- NodeJS/InbuiltPackage/fs.js
+++ NodeJS/InbuiltPackage/fs.js
@@ -17,7 +17,7 @@ const fs =require("fs");
       
      //example--3
     
-    const [, , noofFiles] = process.argv;
+   /* const [, , noofFiles] = process.argv;
     console.log(noofFiles);
     
    const quote3 = "Happy day🥳🥳";
@@ -26,4 +26,19 @@ const fs =require("fs");
     fs.writeFile(`./backup/note-${i}.txt`, quote3, () => {
     console.log(`Completed Writing note-${i}.txt`);
       });
-    }
+    } 
+
+//example-4
+    /* fs.readFile("./cool.txt", "utf-8", (err, data) => {
+    if (err) {
+    console.log("Error❌", err);
+       }
+          console.log("The content of the file is", data);
+         });  */
+        
+        const quote4 = "\nTake Care";
+        
+        fs.appendFile("./nice.txt", quote4, () => {
+          console.log("Completed Writing");
+        });
+        
--- practice/pract.js
+++ practice/pract.js
@@ -1,8 +1,5 @@
-var fs = require("fs");
-var quote = "Make everyday a little less ordinarily";
-fs.writeFile("test-1.txt", quote, () => {
-    console.log("Completed Writing");
-});
-
-var fs = require("fs");
-var quote = "live more worry less";
+const names =["arjun","elbert","eleanora"];
+names.map((data)=>{
+    console.log(data)
+}
+);
