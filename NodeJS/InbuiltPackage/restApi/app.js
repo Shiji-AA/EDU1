bbcadbcd47894652f485fe54cb659826e155c831
Shiji-AA <shijihcl@gmail.com>
2023-03-14 17:08:00 +0530

    changes

--- NodeJS/InbuiltPackage/restApi/app.js
+++ NodeJS/InbuiltPackage/restApi/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongo = require("mongodb");
 const MongoClient = mongo.MongoClient;
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 const MONGO_URL = "mongodb://localhost:27017";
 
@@ -23,7 +23,7 @@ app.get("/locations", function (req, res) {
 MongoClient.connect(MONGO_URL, (err, client) => {
   console.log("Mongo is connected");
   if (err) console.log("Error while connecting");
-  db = client.db("edu1we");
+  var db = client.db("edu1we");
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
