f81f71c0c4b9076a69e3b7fc207127bc1d6c66c7
Shiji-AA <shijihcl@gmail.com>
2023-03-30 13:28:07 +0530

    loginSApp

---
+++ NodeJS/InbuiltPackage/first.js
@@ -0,0 +1,10 @@
+var sum = function(x,y){
+    return x+y;
+}
+var mul =function(x,y) {
+    return x*y;
+};
+module.exports = {
+    add : sum,
+    product : mul
+}
---
+++ NodeJS/InbuiltPackage/second.js
@@ -0,0 +1,2 @@
+var calc = require ("./first.js");
+console.log(calc.product(10,20));
