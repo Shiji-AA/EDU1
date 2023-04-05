c573b0e916cb030723b740e870e859a93fb7d855
Shiji-AA <shijihcl@gmail.com>
2023-04-05 11:51:19 +0530

    chages

--- React/product-app/src/App.js
+++ React/product-app/src/App.js
@@ -1,18 +1,14 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 import "./App.css";
-import {DisplayData} from "./DisplayData";
-import {User} from "./User";
-
-
-
-
+import { User } from "./User.js";
 export default class App extends Component {
 constructor (props){
   super(props);
   this.state = {}
 }
 render() {
-  return <div className="App">User</div> ;
+  return (<div className="App">
+  </div>);
 }
 }
 
